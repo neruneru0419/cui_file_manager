6938c25292675795ddf5ec34932ce6a77f933a31
neruneru0419 <neruneru55555@gmail.com>
2020-04-15 22:56:47 +0900

    ファイル名を受け取る処理途中経過

--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,59 @@ use std::io::{Read, Write, stdin};
 fn main() {
     println!("使用するモードを選んでください");
     println!("1->送信モード 2->受信モード");
-    let mut mode = String::new();
-    stdin().read_line(&mut mode)
-        .expect("Failed to read line");
-    if mode == "1\n"{
+    let mut mode = input();
+    if mode == "1"{
         start_client();
-    }else if mode == "2\n" {
+    }else if mode == "2" {
         start_server();
     }
 }
 
 
-fn start_server(){
 
-    println!("ファイルの受信待ちです...");
-    let ip = "127.0.0.1";
-    let port = "8080";
-    listen((format!("{}:{}", ip, port)), |out| {
-        println!("ファイルを受信しています...");
-        move |msg| {
-            let contents = string_to_vec(format!("{}", msg));
-            let mut file = File::create("hoge.jpg").unwrap();
-            file.write_all(&contents).unwrap();
-            file.flush().unwrap();
-            println!("ファイルの受信が完了しました。");
-            out.send(" ");
-            out.close(CloseCode::Normal)
-        }
-
-    }).unwrap()
-
-}
 
 fn start_client(){
-
-    let ip = "127.0.0.1";
-    let port = "8080";
-    connect((format!("ws://{}:{}", ip, port)), |out| {
-        let mut file_name: String = String::new();
+    println!("接続するサーバーのIPアドレスを入力して下さい");
+    let ip = input();
+    println!("ポートを入力して下さい");
+    let port = input();
+    connect(format!("ws://{}:{}", ip, port), |out| {
         println!("送信するファイル名を入力してください");
-        stdin().read_line(&mut file_name)
-            .expect("Failed to read line");
-        file_name.retain(|chr| chr != '\n');
+        let file_name: String = input();
         let file_binary = get_file_binary(file_name.clone());
+        let file_data = vec![file_name, format!("{:?}", file_binary)];
         println!("ファイルを送信しています...");
-        out.send(format!("{:?}", file_binary)).unwrap(); 
+        out.send(format!("{:?}",file_data)).unwrap(); 
         move |msg| {
             println!("ファイルの送信を完了しました。");
             out.close(CloseCode::Normal)
         }
     }).unwrap()
 
+
+}
+
+fn start_server(){
+
+    println!("サーバーのIPアドレスを入力して下さい");
+    let ip = input();
+    println!("ポートを入力して下さい");
+    let port = input();
+    println!("ファイルの受信待ちです...");
+    listen(format!("{}:{}", ip, port), |out| {
+        println!("ファイルを受信しています...");
+        move |msg| {
+            let file_data = parse_file_data(format!("{}", msg));
+            let file_name = format!("{}", file_data[0]);
+            let file_binary = string_to_vec(format!("{}", file_data[1]));
+            let mut file = File::create(file_name).unwrap();
+            file.write_all(&file_binary).unwrap();
+            file.flush().unwrap();
+            println!("ファイルの受信が完了しました。");
+            out.send(" ")
+        }
+
+    }).unwrap()
 
 }
 
@@ -77,4 +79,18 @@ fn get_file_binary(filename: String) -> Vec<u8>{
     f.read_to_end(&mut file_binary)
         .expect("failed to read file");
     file_binary
+}
+
+fn input() -> String{
+    let mut input_str: String = String::new();
+    stdin().read_line(&mut input_str)
+        .expect("Failed to read line");
+    input_str.retain(|chr| chr != '\n');
+    input_str
+}
+
+fn parse_file_data(mut msg: String) -> Vec<String>{
+    let mut file_data: Vec<String> = msg.split(' ').collect();
+    //ファイル名とバイナリをパース
+    file_data
 }
